6cd04f331daac9b1bea0ced1cac8585fd7f25294
shraddha-cpu <shraddhapaygude5337@gmail.com>
2022-09-10 16:06:48 +0530

    Homework pattern1

---
+++ shraddha1-whileloop/Class5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shraddha1_whileloop
+{
+    internal class Class5
+    {
+        static void Main(string[] args)
+        {
+            int i, j, n;
+            for (i = 1; i<=4; i++)
+            {
+                for (j = 1; j<=5; j++)
+                {
+                    Console.WriteLine("*");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
